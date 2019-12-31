13853162e2cbf4d7433a8d1859e7c08ca102f1da
James Vermilya <jwvermilya@msn.com>
2019-12-30 19:52:10 -0600

    End Section 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Logged In');
     }, error => {
-      console.log('Failed Login');
+      console.log(error);
     });
   }
 
